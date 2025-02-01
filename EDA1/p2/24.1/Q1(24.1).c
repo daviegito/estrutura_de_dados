a5349470beec59febcf2542597294ced6d592a60
Davi do Egito <daviegitocoelho@gmail.com>
2025-02-01 15:36:54 -0300

    Added and modified stuff

--- EDA1/p2/24.1/Q1(24.1).c
+++ EDA1/p2/24.1/Q1(24.1).c
@@ -1,5 +1,6 @@
-//Faz um "fatorial de adição" do último Sobrenome. Coelho = 6 letras.
+//Calcula o comprimento da string. Coelho = 6 letras.
 //6+5+4+3+2+1=21
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,12 +12,6 @@ int Q1(char *Nome, int qtde) {
         pc++;
     return Q1(Nome+1, qtde+pc);
 }
-/*Retorna a letra minúscula do alfabeto correspondente a posição P
-// Letra 'a' corresponde a P = 0, letra 'z' corresponde a P = 25
-char letraCorrespondente(int P){
-    P = P % 26;
-    return 'a' + P; 
-}*/
 
 int main(){
     int res = Q1("Coelho", 0);
---
+++ EDA1/p3/arvore.c
@@ -0,0 +1,9 @@
+#ifndef TNOBIN_H
+#define	TNOBIN_H
+struct TNoBin
+{
+  int info;
+  TNoBin *Esquerda;
+  TNoBin *Direita;
+}	;
+#endif
